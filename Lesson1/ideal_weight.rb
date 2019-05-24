cf1fe3380c128495e5715c0d6237cbaefdafa89f
Kanat Amren <arruah@gmail.com>
2019-05-24 08:21:23 +0600

    Ideal weight add

---
+++ Lesson1/ideal_weight.rb
@@ -0,0 +1,16 @@
+=begin 
+  The program asks the user for a name and height and displays the ideal weight by the formula <height> - 110,
+  and then displays the result to the user on the screen with the treatment by name. If the ideal weight is negative,
+  the line "Your weight is already optimal"
+=end
+
+puts "What is your name?"
+name = gets.chomp
+puts "What is your height (in centimeters)?"
+user_height = gets.chomp
+ideal_weght = user_height.to_i - 110
+if ideal_weght > 0
+  puts " #{name} your ideal weight is #{ideal_weght}"
+else
+  puts "#{name} your weight is already ideal!"
+end
